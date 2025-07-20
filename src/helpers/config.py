67a42e2c63ab344372e16431fbce4e0f44f0078d
Ali Raafat <aliraafat775@gmail.com>
2025-07-20 22:45:30 +0300

    update config file

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -8,6 +8,17 @@ class Settings(BaseSettings):
     FILE_DEFAULT_CHUNK_SIZE:int
     DB_URL:str
     MONGODB_DATABASE:str
+    GENERATION_BACKEND:str
+    EMBEDDING_BACKEND:str
+    OPENAI_API_KEY:str=None
+    OPENAI_URL:str=None
+    COHERE_API_KEY:str=None
+    GENERATION_MODEL_ID:str=None
+    EMBEDDING_MODEL_ID:str=None
+    EMBEDDING_SIZE:int=None
+    INPUT_DEFAULT_MAX_CHARACTERS:int=None
+    GENERATION_DEFAULT_MAX_TOKENS:int=None
+    GENERATION_DEFAULT_TEMPERATURE:float=None
 
     class Config():
         env_file=".env"
