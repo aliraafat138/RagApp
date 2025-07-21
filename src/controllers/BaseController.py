4e618de6c0d4d9d0aa00b1350fd9c57273cdecbc
Ali Raafat <aliraafat775@gmail.com>
2025-07-21 03:11:53 +0300

    update base controller

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -5,3 +5,11 @@ class BaseController:
         self.app_settings=get_settings()
         self.base_dir=os.path.dirname(os.path.dirname(__file__))
         self.file_dir=os.path.join(self.base_dir,"assets/files")
+        self.database_dir=os.path.join(self.base_dir,"assets/files")
+
+    def get_database_path(self,db_name:str):
+        database_path=os.path.join(self.database_dir,db_name)
+
+        if not os.path.exists(database_path):
+            os.makedirs(database_path)
+        return database_path
