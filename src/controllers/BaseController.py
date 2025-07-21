62e19b8a6381b7f95ea65f9443321ee18d49a788
Ali Raafat <aliraafat775@gmail.com>
2025-07-22 01:58:18 +0300

    updated

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -5,7 +5,7 @@ class BaseController:
         self.app_settings=get_settings()
         self.base_dir=os.path.dirname(os.path.dirname(__file__))
         self.file_dir=os.path.join(self.base_dir,"assets/files")
-        self.database_dir=os.path.join(self.base_dir,"assets/files")
+        self.database_dir=os.path.join(self.base_dir,"assets/database")
 
     def get_database_path(self,db_name:str):
         database_path=os.path.join(self.database_dir,db_name)
